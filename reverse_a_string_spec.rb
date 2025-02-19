ddb741a6a95d560072866ad6c6f5d6f97744ac63
ameersohail <ameersohailms@gmail.com>
2025-02-19 19:25:02 +0530

    added some test cases to verify

--- reverse_a_string_spec.rb
+++ reverse_a_string_spec.rb
@@ -7,5 +7,13 @@ describe   do
     it "reverse the String" do
       expect(rs.reverse_string("sohail")).to eq("liahos") 
     end
+    it "length should be same " do
+      expect(rs.reverse_string("ameer").length).to eq(5)
+    end
+    it "should contain all the charcters" do
+      str = "hello"
+      rstr =  rs.reverse_string(str)
+      expect(rstr.char.sort).to eq(str.char.sort)
+    end
   end
 end
